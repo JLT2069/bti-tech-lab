be1e59ba4921baffa6a86a4e2b2ebe2df096515a
JLT <13213931@student.uts.edu.au>
2018-08-08 11:59:55 +1000

    file added

--- Demo excel r.R
+++ Demo excel r.R
@@ -1,1 +1,13 @@
+library(tidyverse)
+library(lubridate)
+
+setwd(~/"Downloads")
+(df1 <- read_csv("Crop_example1.0 TL.csv"))
+df1
+
+df2 <- df1 %>% 
+  gather("Date,"Value", c(=cCrop, -Type"))
+
 View(df2)
+df2 <- df1 %>%
+  gather("Date, "Value, c(-Crop,-Type))
